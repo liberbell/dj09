4e69e6adee2f5a2fe88a2b6a0354443220fd211d
Liberbell <liberbell@gmail.com>
2023-11-13 08:33:41 +0900

    views3

--- helloworldproject/helloworldproject/views.py
+++ helloworldproject/helloworldproject/views.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def someview(request):
     print(Path(__file__).resolve().parent.parent)
@@ -9,3 +10,6 @@ def helloworldfunction(request):
     returned_object = HttpResponse("<h1>Hello World</h1>")
 
     return returned_object
+
+class HelloWorldClass(TemplateView):
+    template_name = "hello.html"
