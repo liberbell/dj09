f404a4e14579831822dbd46242f3c21f252e1dbb
Liberbell <liberbell@gmail.com>
2023-11-12 21:26:04 +0900

    views6

--- helloworldproject/helloworldproject/views.py
+++ helloworldproject/helloworldproject/views.py
@@ -2,5 +2,5 @@ from pathlib import Path
 from django.http import HttpResponse
 
 def someview(request):
-    print(Path(__file__).resolve())
+    print(Path(__file__).resolve().parent.parent)
     return HttpResponse("")
