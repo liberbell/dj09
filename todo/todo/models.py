e70117aaf584b49742060fa7ad05af5c45e6d96f
Liberbell <liberbell@gmail.com>
2023-11-14 08:55:36 +0900

    models3

--- todo/todo/models.py
+++ todo/todo/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class TodoModel(models.Model):
     title = models.CharField(max_length=255)
     memo = models.TextField(max_length=1024)
+
+    def __str__(self):
+        return self.title
+    
