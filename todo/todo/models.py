01c3b3ec640775fc0e7393c50f5a297119ebbb21
Liberbell <liberbell@gmail.com>
2023-11-14 08:43:56 +0900

    models1

--- todo/todo/models.py
+++ todo/todo/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class TodoModel(models.Model):
+    title = models.CharField(max_length=255)
+    memo = models.TextField(max_length=1024)
