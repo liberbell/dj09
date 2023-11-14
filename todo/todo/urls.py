0980e2bde37197310ed8fc8931d7a67f2fcbc7d0
Liberbell <liberbell@gmail.com>
2023-11-15 08:45:52 +0900

    urls1

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -3,5 +3,5 @@ from .views import Todolist, TodoDetail
 
 urlpatterns = [
     path("list/", Todolist.as_view(), name="list"),
-    path("detail/", TodoDetail.as_view(), name="detail"),
+    path("detail/<int:pk>", TodoDetail.as_view(), name="detail"),
 ]
