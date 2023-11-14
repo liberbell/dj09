811a38326f08cf244cfcca079d7e70308e1675a9
Liberbell <liberbell@gmail.com>
2023-11-15 08:26:16 +0900

    urls1

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from django.contrib import admin
+from .views import Todolist
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("lost/", Todolist.as_view(), name="list"),
 ]
