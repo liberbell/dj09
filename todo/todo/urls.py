5601eae75115d925dca891c2fe06df64ced9ea42
Liberbell <liberbell@gmail.com>
2023-11-15 08:39:11 +0900

    urls3

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Todolist
+from .views import Todolist, TodoDetail
 
 urlpatterns = [
     path("list/", Todolist.as_view(), name="list"),
+    path("detail/", TodoDetail.as_view(), name="detail"),
 ]
