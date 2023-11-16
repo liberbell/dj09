3dfb75c085b896c1c24886d1949a01b012bbaf94
Liberbell <liberbell@gmail.com>
2023-11-17 08:43:44 +0900

    models1

--- todo/todo/models.py
+++ todo/todo/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 # Create your models here.
+CHOICE = ("danger", "high")
+
 class TodoModel(models.Model):
     title = models.CharField(max_length=255)
     memo = models.TextField(max_length=1024)
+    priority = models.CharField(max_length=50, choices=CHOICE)
 
     def __str__(self):
         return self.title
