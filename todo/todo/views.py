b20bec4e5f181f730bf3d4977b5e441c8412f176
Liberbell <liberbell@gmail.com>
2023-11-17 09:02:21 +0900

    views3

--- todo/todo/views.py
+++ todo/todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import TodoModel
 
 # Create your views here.
@@ -9,4 +9,8 @@ class Todolist(ListView):
 
 class TodoDetail(DetailView):
     template_name = "detail.html"
+    model = TodoModel
+
+class TodoCreate(CreateView):
+    template_name = "create.html"
     model = TodoModel
