79bf97ed63d9321387e6318278f11dbe56f1a617
Liberbell <liberbell@gmail.com>
2023-11-17 09:01:46 +0900

    urls1

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Todolist, TodoDetail
+from .views import Todolist, TodoDetail, TodoCreate
 
 urlpatterns = [
     path("list/", Todolist.as_view(), name="list"),
     path("detail/<int:pk>", TodoDetail.as_view(), name="detail"),
+    path("create/", TodoCreate.as_view(), name="create"),
 ]
