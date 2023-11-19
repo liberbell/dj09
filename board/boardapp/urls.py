47718c7da9323861fdebebea6436427bab3bca04
Liberbell <liberbell@gmail.com>
2023-11-20 08:35:22 +0900

    urls2

--- board/boardapp/urls.py
+++ board/boardapp/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
 from django.urls import path
+from .views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("signup/", signupfunc, name="signup"),
 ]
