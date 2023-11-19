00e5a4e2478cf86c7e3f10b483eaa1038ab7bb9a
Liberbell <liberbell@gmail.com>
2023-11-19 11:37:47 +0900

    views1

--- todo/todo/views.py
+++ todo/todo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from .models import TodoModel
 
 # Create your views here.
@@ -23,3 +23,7 @@ class TodoDelete(DeleteView):
     template_name = "delete.html"
     model = TodoModel
     success_url = reverse_lazy("list")
+
+class TodoUpdate(UpdateView):
+    template_name = "update.html"
+    model = TodoModel
