fbe34bf19afc74eaa9179407038d9923b7759516
Liberbell <liberbell@gmail.com>
2023-11-21 08:42:48 +0900

    views1

--- board/boardapp/views.py
+++ board/boardapp/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def signupfunc(request):
+
+    if request.method == "POST":
+        print("This is post method.")
+    else:
+        print("This is get method.")
     return render(request, "signup.html", {
         "some": 100
     })
