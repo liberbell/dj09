86e7a020709b7a08ae1083a0fcc553408e4aa5ca
Liberbell <liberbell@gmail.com>
2023-11-22 08:22:37 +0900

    views1

--- board/boardapp/views.py
+++ board/boardapp/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your views here.
 def signupfunc(request):
     object_list = User.objects.all()
+    print(object_list)
 
     if request.method == "POST":
         print("This is post method.")
