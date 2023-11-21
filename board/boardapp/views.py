1c777052ae9737c84f3140324f11de1478e890c5
Liberbell <liberbell@gmail.com>
2023-11-22 08:33:50 +0900

    views3

--- board/boardapp/views.py
+++ board/boardapp/views.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def signupfunc(request):
-    print(request.POST)
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = User.objects.create_user(username, "", password)
     return render(request, "signup.html", {
         "some": 100
     })
