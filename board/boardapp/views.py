e28ca6884842a54a9fc86c466568432c6191fe51
Liberbell <liberbell@gmail.com>
2023-11-26 13:38:09 +0900

    views4

--- board/boardapp/views.py
+++ board/boardapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import authenticate, login, logout
@@ -15,7 +15,7 @@ def signupfunc(request):
             })
         except IntegrityError:
             return render(request, "signup.html", {"error": "User already exists"})
-    return render(request, "signup.html", {
+    return render(request, "login.html", {
         "some": 100
     })
 
