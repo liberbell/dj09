617e1f9d35bb6993cdffe308725a866ba77de1b7
Liberbell <liberbell@gmail.com>
2023-11-26 13:28:47 +0900

    views2

--- board/boardapp/views.py
+++ board/boardapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def signupfunc(request):
@@ -24,3 +24,5 @@ def loginfunc(request):
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
