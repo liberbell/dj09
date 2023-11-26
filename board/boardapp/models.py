89f021a1c976e7ba789787358b72f0ef16b7a3b1
Liberbell <liberbell@gmail.com>
2023-11-27 08:36:23 +0900

    models1

--- board/boardapp/models.py
+++ board/boardapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class BoardModel(models.Model):
+    title = models.CharField(max_length=1024)
+    content = models.TextField()
+    author = models.CharField(max_length=50)
+    sns_image = models.ImageField(upload_to="")
+    good = models.IntegerField()
+    read = models.IntegerField()
+    read_text = models.TextField()
