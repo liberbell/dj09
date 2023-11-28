9599f237a08272100fc81009e1b2cb820f6a40f5
Liberbell <liberbell@gmail.com>
2023-11-29 08:43:06 +0900

    views4

--- board/boardapp/views.py
+++ board/boardapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_list_or_404
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import authenticate, login, logout
@@ -43,5 +43,6 @@ def logoutfunc(request):
     logout(request)
     return redirect("login")
 
-def detailfunc(request):
-    pass
+def detailfunc(request, pk):
+    object = get_list_or_404(BoardModel, pk=pk)
+    return render(request, "detail.html", {"object": object})
